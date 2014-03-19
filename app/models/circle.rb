7767b4cc875d435d545d8b493befa86e0f11774b
shellyagarwal <shellyagarwal@codingmart.com>
2014-03-19 17:52:29 +0530

    All bugs fixed

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,4 +1,5 @@
 class Circle < ActiveRecord::Base
 	has_and_belongs_to_many :users
+	belongs_to :user
 	validates :circle_name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-  has_and_belongs_to_many :circles
+  has_and_belongs_to_many :members , :class_name => "Circle"
+  has_many :circles
 
 end
